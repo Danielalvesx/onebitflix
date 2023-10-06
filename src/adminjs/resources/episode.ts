c2a77dc3441c162bfa4761f064cefed849c08982
Danielalvesx <daniel.alves147852@gmail.com>
2023-10-05 23:12:53 -0300

    Criando Tabela de Episódios

---
+++ src/adminjs/resources/episode.ts
@@ -0,0 +1,15 @@
+// src/adminjs/resources/episode.ts
+
+import { ResourceOptions, ResourceWithOptions } from 'adminjs';
+import { adminJs } from '../index';
+import { adminJsResources } from './index';
+import { categoryResourceOptions } from './category';
+import { courseResourceOptions } from './course';
+
+export const episodeResourceOptions: ResourceOptions = {
+  navigation: 'Catálogo',
+  editProperties: ['name', 'synopsis', 'courseId', 'order', 'uploadVideo', 'secondsLong'  ],
+  filterProperties: ['name', 'synopsis', 'courseId', 'secondsLong', 'createdAt', 'updatedAt'],
+  listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'],
+  showProperties: ['id', 'name', 'synopsis', 'courseId', 'order', 'videoUrl', 'secondsLong', 'createdAt', 'updatedAt']
+}
